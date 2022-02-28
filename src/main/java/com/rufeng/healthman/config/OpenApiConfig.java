34bc6d6dbbbb4f9d3f3e04f6b91b1503655b039e
rufeng-h <chunfengh@163.com>
2022-02-28 10:14:57 +0800

    准备工作完成

---
+++ src/main/java/com/rufeng/healthman/config/OpenApiConfig.java
@@ -0,0 +1,9 @@
+package com.rufeng.healthman.config;
+/**
+ * @author rufeng
+ * @time 2022-02-24 10:09
+ * @package com.rufeng.healthman.config
+ * @description TODO
+ */
+public class OpenApiConfig {
+}
