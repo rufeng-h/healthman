e6c48580075b149fd5758f975de7cdf63d8ef970
rufeng-h <chunfengh@163.com>
2022-02-28 10:16:18 +0800

    准备工作完成

--- src/main/java/com/rufeng/healthman/config/OpenApiConfig.java
+++ src/main/java/com/rufeng/healthman/config/OpenApiConfig.java
@@ -1,9 +1,29 @@
 package com.rufeng.healthman.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 /**
  * @author rufeng
  * @time 2022-02-24 10:09
  * @package com.rufeng.healthman.config
  * @description TODO
  */
+@Profile("dev")
+@Configuration
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .contact(new Contact().email("chunfengh163@163.com").name("rufeng").url("http://windcf.com"))
+                        .description("兴趣所至")
+                        .title("体质健康系统")
+                        .license(new License().url("https://opensource.org/licenses/MIT")).version("v1.0"));
+    }
 }
--- src/main/java/com/rufeng/healthman/controller/test/TestController.java
+++ src/main/java/com/rufeng/healthman/controller/test/TestController.java
@@ -4,6 +4,9 @@ import com.rufeng.healthman.common.ApiPage;
 import com.rufeng.healthman.common.ApiResponse;
 import com.rufeng.healthman.domain.User;
 import com.rufeng.healthman.service.test.TestService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +21,9 @@ import java.util.Map;
  * @author rufeng
  * @time 2022-01-10 15:24
  * @package com.rufeng.healthman.controller.test
- * @description TODO
+ * @description 测试接口
  */
+@Tag(name = "test", description = "测试接口")
 @RestController
 @Validated
 @RequestMapping("/test")
@@ -30,15 +34,17 @@ public class TestController {
         this.testService = testService;
     }
 
+    @Operation()
     @GetMapping("/all")
     public List<Map<String, Object>> getAll() {
         return testService.getAll();
     }
 
     @GetMapping("/page")
+    @Operation(description = "分页查询", summary = "分页")
     public ApiResponse<ApiPage<User>> testPage(
-            @RequestParam(defaultValue = "1") @Range(min = 1) Integer pageNum,
-            @RequestParam(defaultValue = "5") Integer pageSize) {
+            @Parameter(description = "每页条数") @RequestParam(defaultValue = "1") @Range(min = 1) Integer pageNum,
+            @Parameter(description = "当前页数") @RequestParam(defaultValue = "5") Integer pageSize) {
         return ApiResponse.success(testService.selectPage(pageNum, pageSize));
     }
 }
